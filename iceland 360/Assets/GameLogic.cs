e212d9e9260fa4a0d8f80634dabcaf7bcc09b1da
haneentalawneh <haneen_t_alawneh@hotmail.com>
2018-11-24 12:18:38 +0200

    Initial commit

---
+++ iceland 360/Assets/GameLogic.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GameLogic : MonoBehaviour
+{
+	public GameObject[] screens;
+	public VideoClip[] movies;
+	public GameObject movieScreen;
+	public GameObject rain;
+	VideoPlayer videoPlayer;
+	public GameObject play;
+	public GameObject pause;
+	public GameObject playAgain;
+	public GameObject ControlScreen;
+	public GvrAudioSource src;
+	int index = 0;
+	bool isFinished = false;
+	int currentTime = 0;
+
+	void Start ()
+	{
+		videoPlayer = movieScreen.GetComponent<VideoPlayer> ();
+	}
+
+	void Update ()
+	{
+		Debug.Log("frame:" + videoPlayer.time);
+		Debug.Log ((double)videoPlayer.url.Length);
+
+		if (!videoPlayer.isPlaying && !isFinished) {
+			if (videoPlayer != null) {
+				if (videoPlayer.frame != null) {
+					
+					if ((double)videoPlayer.url.Length == videoPlayer.time) {
+						//Video has finshed playing!
+					
+						toggleObject (playAgain);
+						isFinished = true;
+					}
+				}
+			}
+		}
+	}
+
+	public void onStartClicked ()
+	{
+		toggleObject (screens [0]);
+		toggleObject (screens [1]);
+	
+	}
+
+	public void onActionClicked ()
+	{
+		toggleObject (screens [1]);
+		toggleObject (screens [2]);
+
+	}
+
+	public void StartMovie (int index)
+	{
+		this.index = index;
+		toggleMenu ();
+		PlayMovie ();
+
+	}
+
+	//public void
+
+	public void OnPauseClicked ()
+	{
+		src.Stop ();
+		if (videoPlayer != null) {
+			
+			if (videoPlayer.isPlaying) {
+				videoPlayer.Pause ();
+			
+			} else {
+				videoPlayer.Play ();
+			}
+
+
+			toggleObject (play);
+			toggleObject (pause);
+		}
+	}
+
+	void toggleObject (GameObject obj)
+	{
+		obj.SetActive (!obj.activeSelf);
+		
+	}
+
+	public void PlayMovie ()
+	{	
+		isFinished = false;
+		playAgain.SetActive (false);
+
+		//videoPlayer.clip = movies [this.index];
+		videoPlayer.Prepare ();
+		videoPlayer.Play ();
+	
+	}
+
+	public void toggleMenu ()
+	{
+		toggleObject (ControlScreen);
+		toggleObject (screens [0]);
+		toggleObject (rain);
+		toggleObject (movieScreen);
+
+		if (movieScreen.activeSelf == true) {
+			src.Play ();
+		} else {
+			src.Stop ();
+		}
+	}
+
+}
---
+++ iceland 360/README.md
@@ -0,0 +1,27 @@
+# iceland 360
+
+Iceland 360 :
+
+This project is part of the udacitu VR nanodegree 
+
+This game takes you to into a vr journey to iclfnad, in your trip you can either go to Iceland waterfalls or to geysir
+
+Steps done to create this project :
+
+1. Footage is chosen using this repo by udacity: https://www.dropbox.com/sh/er4rc58iuter5br/AACDKrogCISFLobz8IpPmMsqa?dl=0
+
+2. Autopano video +. Giga were used to stitch stabilize, and synchronize the footage to get the best results possible.
+
+2. Stitched video were edited using Premier
+
+How much time did it take ? 
+
+1 week 
+
+What did I like about it ? 
+
+I liked the stitching and editing process as it's something totally new to me to work on creating 360 videos. 
+
+Challenges :
+
+Streaming video from the internet in unity video player, youtube doesn't work, I had to use dropbox but the video rendering is a bit slow, so one of the videos is streamed online and the other is local. 
